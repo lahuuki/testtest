ad865eba7bd3db3264d1811d69efbcf6aee11905
lahuuki <lahuuki@gmail.com>
2020-05-18 14:34:54 -0400

    Add github set up commands

--- dev/02_git_github_setup.R
+++ dev/02_git_github_setup.R
@@ -13,6 +13,8 @@ usethis::use_git() ## Choose the option to make the commit, then to restart RStu
 ## After the restart, continue by connecting your local git repository to
 ## GitHub. You might want to use the `organisation` and `private` arguments
 args(usethis::use_github)
+usethis::browse_github_token()
+usethis::edit_r_environ()
 usethis::use_github()
 ## Follow any prompts, such as running: git push --set-upstream origin master
 
